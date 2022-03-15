0be9bc70264cb177965414201b86f576aa67a592
q1594585592 <1090415804@qq.com>
2022-03-15 08:41:39 +0800

    添加购物车推荐商品

--- src/pages/cart/index.vue
+++ src/pages/cart/index.vue
@@ -5,24 +5,49 @@
       left-text=""
       left-arrow
       @click-left="onClickLeft"
-      @click-right="onClickRight"
     />
     <van-empty
       class="custom-image"
       image="https://cdn.cnbj1.fds.api.mi-img.com/mijia-m/production/yrn-buz-shop-center/res/images/no_result/no_result_cart3.png"
       description="目前没有添加商品哦~"
     />
+    <span class="text" @click="tohome">去首页逛逛 </span>
+    <!-- 列表 -->
+    <van-list
+      v-model="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+      <div class="tuijian">为你推荐</div>
+      <div
+        class="addcart"
+        v-for="item in products"
+        :key="item._id + Math.random()"
+        @click="godetail(item._id)"
+      >
+        <img :src="item.coverImg" alt="" />
+        <div class="product_r">
+          <p>{{ item.name }}</p>
+          <p>{{ item.price }}元</p>
+        </div>
+      </div>
+    </van-list>
   </div>
 </template>
 
 <script>
-import { Toast } from "vant";
-import Vue from "vue";
-import { NavBar } from "vant";
-Vue.use(NavBar);
+import { reqProducts } from "../../api/product";
 export default {
   data() {
-    return {};
+    return {
+      list: [],
+      products: [],
+      banners: [],
+      loading: false, //加载
+      finished: false,
+      page: 1,
+    };
   },
   computed: {},
   watch: {},
@@ -32,8 +57,22 @@ export default {
       this.$router.push("/home");
       Toast("返回");
     },
-    onClickRight() {
-      Toast("按钮");
+    tohome() {
+      this.$router.push("/home");
+    },
+    async getProducts() {
+      this.loading = true;
+      const result = await reqProducts({ page: this.page });
+      this.loading = false;
+      this.products = [...this.products, ...result.products];
+      if (result.products.length < 10) {
+        this.finished = true;
+      } else {
+        this.page++;
+      }
+    },
+    onLoad() {
+      this.getProducts();
     },
   },
   created() {},
@@ -41,9 +80,52 @@ export default {
   components: {},
 };
 </script>
-<style>
-.custom-image .van-empty__image {
-  width: 86px;
-  height: 86px;
+<style scoped>
+.tuijian {
+  font-size: 16px;
+  color: rgb(35, 34, 34);
+  font-weight: 800;
+  margin-top: 70px;
+  margin-left: 42%;
+  margin-bottom: 20px;
+}
+.text {
+  display: block;
+  border: 1px gray solid;
+  width: 101px;
+  height: 37px;
+  border-radius: 25px;
+  text-align: center;
+  line-height: 37px;
+  font-size: 14px;
+  color: rgb(102, 102, 102);
+  position: absolute;
+  left: 50%;
+  margin-left: -50px;
+}
+.van-swipe-item,
+.van-swipe-item img,
+.my-swipe {
+  height: 400px;
+}
+.van-swipe-item img {
+  width: 100%;
+}
+.addcart {
+  padding: 10px;
+  width: 50%;
+  height: 300px;
+}
+.addcart img {
+  width: 160px;
+  height: 160px;
+  border-radius: 10px;
+}
+.addcart p {
+  padding-left: 10px;
+  padding-right: 10px;
+}
+.van-list {
+  padding-bottom: 40px;
 }
 </style>
