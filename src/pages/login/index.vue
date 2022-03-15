c89faf73f88ab0169902bdc11904d36333387d71
Yanlongji0103 <2562963859@qq.com>
2022-03-15 20:45:47 +0800

    登录页面提交按钮修改

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -39,7 +39,7 @@
 
 <script>
 import { reqLogin } from "../../api/user";
-import { Notify } from "vant";
+import { Notify, Toast } from "vant";
 import { setToken } from "../../utils/auth";
 
 export default {
@@ -65,6 +65,8 @@ export default {
         // // vuex持久化
         localStorage.setItem("token", result.token);
         this.$router.push("/home");
+      } else {
+        Toast("请检查账号或密码或进行注册");
       }
     },
     zhuce() {
