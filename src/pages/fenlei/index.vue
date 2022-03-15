6b31a35e3557f1cc1d63e9da642f260d2757cd28
雪瑞 <1036066648@qq.com>
2022-03-15 15:13:38 +0800

    杜雪瑞第二版

--- src/pages/fenlei/index.vue
+++ src/pages/fenlei/index.vue
@@ -3,145 +3,169 @@
     <div class="header">
       <p class="hh">分类 <van-icon name="search" /></p>
     </div>
-<van-sidebar v-model="activeKey" class="left"  @click="bgChange">
-  <van-sidebar-item v-for="item in list" :key="item._id" :title="item.name"  @click="fenleiList2(item.name)"  />
-</van-sidebar>
-<div class="right">
-  <img src="./img/分类1.png" alt="">
-  <div class="top" >
-    <p>精选分类</p>
-    <div class="tt" >
-    <div class="tc" v-for="data in list2.slice(0,6)" :key="data._id" @click="fenleiList2(data.name)"> <img :src="data.coverImg" alt="">
-    <p>{{data.name}}</p>
+    <van-sidebar v-model="activeKey">
+      <van-sidebar-item
+        v-for="item in list"
+        :key="item._id"
+        :title="item.name"
+        @click="fenleiList2(item.name)"
+      />
+    </van-sidebar>
+    <div class="right">
+      <img src="./img/分类1.png" alt="" />
+      <div class="top">
+        <p>精选分类</p>
+        <div class="tt">
+          <div
+            class="tc"
+            v-for="data in list2.slice(0, 6)"
+            :key="data._id"
+            @click="fenleiList2(data.name), godetail()"
+          >
+            <img :src="data.coverImg" alt="" />
+            <p>{{ data.name }}</p>
+          </div>
+        </div>
+      </div>
+      <div class="top">
+        <p>精选专区</p>
+        <div class="tt">
+          <div
+            class="tc"
+            v-for="data in list2.slice(0, 6)"
+            :key="data._id"
+            @click="fenleiList2(data.name), godetail()"
+          >
+            <img :src="data.coverImg" alt="" />
+            <p>{{ data.name }}</p>
+          </div>
+        </div>
+      </div>
     </div>
-   
-  </div>
-  </div>
-  <div class="top">
-    <p>精选专区</p>
-    <div class="tt" >
-    <div class="tc" v-for="data in list2.slice(0,6)" :key="data._id" @click="fenleiList2(data.name)"> <img :src="data.coverImg" alt="">
-    <p>{{data.name}}</p>
-    </div>
-   
-  </div>
-  </div>
-</div>
   </div>
 </template>
 
 <script>
-import{ reqProduct_categories,reqProduct_catelist } from "../../api/product";
+import { reqProduct_categories, reqProduct_catelist } from "../../api/product";
 export default {
- 
   data() {
-  
     return {
-        activeKey: 0,
-       list:[],
-        list2:[]
+      activeKey: 0,
+      list: [],
+      list2: [],
     };
   },
   computed: {},
   watch: {},
 
   methods: {
-     async fenleiList(){
-           const result=await reqProduct_categories();
-           this.list=result.categories
-          //  console.log(result);
-          //  console.log(this.list);
-       } ,
-       async fenleiList2(name){
-        //  console.log(1111,name);
-           const result=await reqProduct_catelist(name);
-           console.log(result);
-           this.list2=result.products;
-       },
-         godetail(id){
-            this.$router.push('/detail/'+id);
-        },
-        bgChange(){
-        //  document.getElementsByClassName("left").style=
-        }
+    async fenleiList() {
+      const result = await reqProduct_categories();
+      this.list = result.categories;
+      //  console.log(result);
+      //  console.log(this.list);
+    },
+    async fenleiList2(name) {
+      console.log(1111, name);
+      const result = await reqProduct_catelist(name);
+      //  console.log(result);
+      this.list2 = result.products;
+      //  this.reqProduct_catelist();
+    },
+    godetail(id) {
+      // console.log(id);
+      this.$router.push("/detail/" + id);
+    },
+    //onLoad默认执行一次
+    onLoad(name) {
+      // console.log(name);
+      this.fenleiList2(name);
+    },
   },
   created() {
     this.fenleiList();
-  
+    this.onLoad("智能手表");
   },
   mounted() {},
   components: {},
 };
 </script>
 <style scoped>
-.header{
+.header {
   width: 100%;
   height: 100px;
   line-height: 100px;
   font-size: 18px;
 }
-.hh{
+.hh {
   text-align: center;
   height: 100px;
   margin-top: -30px;
-    
+  margin-left: 150px;
 }
-.left{
+.van-sidebar {
+  width: 80px;
   float: left;
-  color: #929ea3;
 }
-.right{
+.van-sidebar-item {
+  width: 80px;
+  float: left;
+  color: #a2b0b7;
+}
+.right {
   width: 270px;
   float: right;
-  color: #929ea3;
+  color: #a2b0b7;
 }
-
-.right img{
+.colo {
+  color: red;
+}
+.right img {
   width: 260px;
   height: 100px;
   border-radius: 10px;
 }
-.right .top{
+.right .top {
   width: 270px;
   height: 200px;
-
 }
-.right .top p{
+.right .top p {
   width: 270px;
   height: 30px;
   line-height: 30px;
 }
-.tt{
+.tt {
   width: 270px;
   height: 150px;
-  margin-top: -26px
+  margin-top: -26px;
 }
-.top img{
+.top img {
   margin-top: 20px;
   width: 80px;
   height: 80px;
   float: left;
 }
-.tc{
+.tc {
   width: 70px;
   height: 70px;
   float: left;
   margin: 5px 9px;
 }
-.tc img{
+.tc img {
   width: 50px;
   height: 50px;
 }
-.tc p{
-  
+.tc p {
   height: 12px;
   font-size: 12px;
   clear: both;
- max-width: 60px;
-overflow: hidden;
-text-overflow: ellipsis;
-white-space: nowrap;
+  max-width: 60px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 }
-
-
+.van-icon {
+  margin-left: 100px;
+  font-size: 25px;
+}
 </style>
